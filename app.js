9f783783fb573834dfebb8c05a2ca6db9f145bc3
coderanikey <110382784+coderanikey@users.noreply.github.com>
2026-01-20 14:24:29 +0530

    jhjf

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://locaddssdlhost:${PORT}`);
 });
